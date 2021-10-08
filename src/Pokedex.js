1f2602c62cd0df449e839ece8e3c9fb3c594fd86
Lucas Loureiro <lucas-loureiro@outlook.com>
2021-10-08 11:59:36 -0300

    exercise 2 done

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -3,13 +3,22 @@ import Pokemon from './Pokemon';
 // import Button from './Button';
 
 class Pokedex extends React.Component {
-  constructor() {
-		super();
+  constructor(props) {
+		super(props);
 		this.state = {
 			pokemonIndex: 0,
+			pokemonList: this.props.pokemons,
 		}
 	}
 	
+	pokemonFilter = (pokemonType) => {
+		this.setState((_current, props) => {
+			return {
+				pokemonList: props.pokemons.filter((item) => item.type === pokemonType),
+			}
+		})
+	}
+
 	nextButtonHandler = (array) => {
 		this.setState((current, _props) => {
 			if (current.pokemonIndex === array.length - 1) {
@@ -26,8 +35,16 @@ class Pokedex extends React.Component {
 	render() {
     return (
 			<div className="pokedex">
-				<Pokemon key={this.props.pokemons[this.state.pokemonIndex].id} pokemon={this.props.pokemons[this.state.pokemonIndex]} />
-				<button onClick={() => this.nextButtonHandler(this.props.pokemons)}>Next</button>
+				<Pokemon key={this.state.pokemonList[this.state.pokemonIndex].id} pokemon={this.state.pokemonList[this.state.pokemonIndex]} />
+
+				{/* BUTTON NEXT POKEMON */}
+				<button onClick={() => this.nextButtonHandler(this.state.pokemonList)}>Next</button>
+
+				{/* BUTTON FIRE */}
+				<button onClick={() => this.pokemonFilter('Fire')}>Fire</button>
+
+				{/* BUTTON PSYCHIC */}
+				<button onClick={() => this.pokemonFilter('Psychic')}>Psychic</button>
 			</div>
     );
   }
